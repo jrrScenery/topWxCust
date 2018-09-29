c380b52efba5cf4ccc089f3bace3b3721de411a9
jingrr <jingrr@dcits.com>
2018-09-29 16:20:15 +0800

    微信客户版修改

--- src/views/home/eventEvaluationShow.vue
+++ src/views/home/eventEvaluationShow.vue
@@ -11,7 +11,9 @@
                             <div class="star">
                                 <span class="starTit">{{i+1}}.{{item.question.questionComment}}</span>
                                 <el-rate v-if="item.question.questionComment2"
-                                        v-model="item.scoreval">
+                                        v-model="item.scoreval"
+                                        disabled
+                                        :colors="['#666666', '#999999', '#FF9900']">
                                 </el-rate>
                             </div>
                         </el-form-item>
@@ -20,30 +22,26 @@
                             <div class="improveCell">
                                 <el-form-item>
                                     <el-checkbox-group v-model="item.aroptschked">
-                                        <el-checkbox v-for="itemoption in item.options" :key="itemoption.optionId" :label="itemoption.optionId" >{{itemoption.optionComment}}</el-checkbox>
+                                        <el-checkbox disabled v-for="itemoption in item.options" :key="itemoption.optionId" :label="itemoption.optionId" >{{itemoption.optionComment}}</el-checkbox>
                                     </el-checkbox-group>
                                 </el-form-item>
                             </div>
                         </div>
-                        <el-form-item v-else-if="item.question.questionComment2==null">
-                            <el-input type="textarea" v-model="formData.otherResult" placeholder="请输入其他想法和建议"></el-input>
-                        </el-form-item>
                     </div>
-                    <div style="margin:0.1rem">客户签名</div>
-                    <div v-if="formData.imgStr">
-                        <img style="height:1.5rem;" v-bind:src="formData.imgStr" alt="">
-                    </div>
-                    <div v-else><img style="height:0.5rem;" src="" alt=""></div>
-                    <add-signature :title="addSignatureTit" :queryData="searchData" @searchPro="signature"></add-signature>
+
                     <ul class="signature">
-                        <el-form-item label="工程师">
-                            <div>{{formData.engineername}}</div>
-                        </el-form-item>
-                    </ul>
-                    <div style="height: 0.6rem;"></div>
-                    <el-form-item class="serviceSubmitBtn" v-if="!signImg">
-                        <el-button @click="submitForm('formData')">提交</el-button>
-                    </el-form-item>
+                        <li class="lisign">
+                            <span>客户签字</span>
+                            <div class="sign">
+
+                            <img :src="imgStr">
+                            </div>
+                        </li>
+                        <li>
+                            <span>工程师</span>
+                            <div>{{engineername}}</div>
+                        </li>
+                    </ul>                  
                 </el-form>
                 
             </div>
@@ -54,31 +52,17 @@
 <script>
 import headerLast from '../header/headerLast'
 import fetch from '../../utils/ajax'
-import addSignature from '../../components/addSignature'
 export default {
     name:'serviceRate',
     components:{
         headerLast,
-        addSignature
     },
     data(){
         return{
             serviceRateTit:'服务评价',
-            addSignatureTit:'添加签名',
-            searchData:[],
-            formData:{
-                optionOption:[],
-                question:[],
-                scoreOption:[],
-                imgStr:'',
-                aroptschked:[],
-                otherResult:'',
-                engineername:''
-            },
-            score:[],
             evaluateval:[],
-            scoreOption:[],
-            signImg:"",
+            imgStr:"",
+            engineername:'',
             workId:this.$route.query.workId,
             caseId:this.$route.query.caseId,
             evaluateId:this.$route.query.evaluateid
@@ -90,8 +74,7 @@ export default {
         fetch.get("?action=GetCaseEvaluateInfo&EVALUATE_ID=" + this.evaluateId).then(res=>{
             console.log(res);
             if(res.imgObject!=null){
-                this.formData.imgStr = res.imgObject.imgStr;
-                this.signImg = res.imgObject.imgStr;
+                this.imgStr = res.imgObject.imgStr;
             }
             if(res.STATUSCODE==0){
                 this.scoreOption = res.scoreOption;
@@ -118,10 +101,7 @@ export default {
             }
         })         
     },
-    methods:{           
-        signature(imgStr){
-            this.formData.imgStr = imgStr;
-        },
+    methods:{    
         getScore(scoreOption,questionId){
             var score = 0;
             scoreOption.forEach(function(v,i,ar){
@@ -133,125 +113,6 @@ export default {
                 }
             })
             return score;
-        },
-        getScoreOpinionId(scoreOption,questionId,scoreval){
-            console.log("bbbbbbbbb");
-            console.log("questionId:"+questionId);
-            console.log("scoreval："+scoreval)
-            let optionId;
-            scoreOption.forEach(function(v,i,ar){
-                if(v.questionId == questionId&&v.optionScore==scoreval){
-                    optionId = v.optionId;
-                    return false;
-                }
-            })
-            console.log("optionId:"+optionId);
-            return optionId;
-        },
-        submitForm(formName){
-            const loading = this.$loading({
-                lock: true,
-                text: '提交中...',
-                spinner: 'el-icon-loading',
-                background: 'rgba(255, 255, 255, 0.3)'
-            });
-            let vm= this;
-            this.$refs[formName].validate((valid) => {
-                if(valid){
-                    let detailArray = new Array();
-                    var totalScore = 0;
-                    var failFlg = 0;
-                    var countScore = 0;
-                    var returnFlg = 0;
-                    vm.evaluateval.forEach(function(v,i,ar){
-                        let options = v.options;                        
-                        if(v.question.questionComment2){
-                            if(v.scoreval>0){
-                                totalScore+=v.scoreval;
-                                countScore += 1;
-                                if(v.scoreval<3) failFlg = 1;
-                                var scoreOptionId = vm.getScoreOpinionId(vm.scoreOption,v.question.questionId,v.scoreval);
-                                    var temp1 = {};     
-                                        temp1.evaluateId=vm.evaluateId;
-                                        temp1.questionId=v.question.questionId;
-                                        temp1.optionId=scoreOptionId;   
-                                        detailArray.push(temp1);
-                            }else{
-                                returnFlg = 1;
-                                vm.$message({
-                                    message:'请给相关项打分',
-                                    type: 'warning',
-                                    center: true,
-                                    customClass:'msgdefine'
-                                });
-                                loading.close();
-                                return false;
-                            }
-                        }     
-                        console.log("pppppppppppp");
-                        console.log(v.aroptschked);           
-                        v.aroptschked.forEach(function(item,i,ar){
-                            options.forEach(function(items,i,ar){
-                                if(items.optionId == item){
-                                    let temp2 = {};
-                                    temp2.evaluateId = vm.evaluateId;
-                                    temp2.questionId = items.questionId;
-                                    temp2.optionId = item;
-                                    detailArray.push(temp2);
-                                }
-                            }) 
-                        })      
-                        if(vm.formData.otherResult){
-                            if(v.question.questionComment2==null){
-                                var temp3={};
-                                temp3.evaluateId = vm.evaluateId;
-                                temp3.questionId = v.question.questionId;
-                                temp3.otherResult=vm.formData.otherResult;
-                                detailArray.push(temp3);
-                            }
-                        }
-                    })
-                    if(returnFlg){
-                        loading.close();
-                        return;
-                    }
-                    if(!vm.formData.imgStr){
-                        vm.$message({
-                            message:'请签名',
-                            type: 'warning',
-                            center: true,
-                            customClass:'msgdefine'
-                        });
-                        loading.close();
-                        return;
-                    }
-                    let avgScore = totalScore/countScore;
-                    let postData = new URLSearchParams;
-                    postData.append('evaluateStatus',2);
-                    postData.append('evaluateId',vm.evaluateId);
-                    postData.append('totalScore',avgScore);
-                    postData.append('EvaluateResult',JSON.stringify(detailArray));
-                    postData.append('failFlg',failFlg);
-                    postData.append('imgStr',this.formData.imgStr);
-                    console.log("mmmmmmmmm");
-                    console.log(detailArray);
-                    fetch.post("?action=/case/SubmitClientReview",postData).then(res=>{
-                        console.log(res);
-                        loading.close();
-                        if(res.STATUSCODE=="0"){
-                            let nowcaseId = vm.caseId;
-                            setTimeout(function(){vm.$router.push({name: 'caseEvaluateList',query:{caseId:nowcaseId}})},1000);
-                        }else{
-                            this.$message({
-                                message:res.MESSAGE+"发生错误",
-                                type: 'error',
-                                center: true,
-                                customClass: 'msgdefine'
-                            });
-                        }                       
-                    })
-                }
-            })
         },
     } 
 }
@@ -270,10 +131,12 @@ export default {
 .improveCell span{ color: #666;word-wrap: break-word}
 .el-checkbox__input.is-disabled+span.el-checkbox__label{color: #666!important;}
 
-.signature .el-form-item{overflow: hidden;zoom:1; margin: 0; color: #2698d6; border-top: 0.01rem solid #e1e1e1;border-bottom: 0.01rem solid #e1e1e1}
-.signature .el-form-item div{color: #333333; margin-left: 0.6rem;}
-.signature .el-form-item .sign{}
-.signature .el-form-item .sign img{width: 100%; display: block; flex-grow: 1;}
+.signature li{overflow: hidden;zoom:1; margin: 0; padding: 0.1rem 0;  color: #999999; border-bottom: 0.01rem solid #e1e1e1}
+.signature li span{ width: 0.6rem; float: left;}
+.signature li div{color: #333333; margin-left: 0.6rem;}
+.signature li .sign{ }
+.signature li .sign img{width: 100%; display: block; flex-grow: 1;}
+
 .serviceSubmitBtn >>> .el-form-item__content{margin: 0!important;}
 .serviceSubmitBtn >>> .el-form-item__content .el-button{width: 100%; border: 0.01rem solid #2698d6; background: #2698d6; border-radius: 0; font-size: 0.16rem; color: #ffffff; height: 0.5rem; position: absolute; bottom: 0;}
 </style>
